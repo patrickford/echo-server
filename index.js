960e34f63777eb182d3930736265f4fbceadf658
Patrick Ford <patrickford@msn.com>
2016-08-23 21:07:14 -0700

    Fixed request header

--- index.js
+++ index.js
@@ -9,4 +9,23 @@ app.get('/', function(request, response) {
     });
 });
 
+app.get('/user/:firstname/:lastname', function(request, response) {
+  var first = request.params.firstname;
+  var last = request.params.lastname;
+  response.send(["Hello", first, last].join(" "));
+});
+
+app.get('/jedi/:firstname/:lastname', function(request, response) {
+  var first = request.params.firstname;
+  var last = request.params.lastname;
+  response.send("Hello " + last.slice(0,2) + first.slice(0,3));
+});
+
+app.get('/headers/:header', function(request, response) {
+  var header = request.params.header;
+  // headerInfo.url = request.url;
+  // headerInfo.hostname = request.hostname;
+  response.json(request[header]);
+});
+
 app.listen(8080);
